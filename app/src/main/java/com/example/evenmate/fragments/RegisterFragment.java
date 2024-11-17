39a0145a13ddba7eda8443cf2b42a11a9c8630c8
zoricnadja <zoric.n2003@gmail.com>
2024-11-17 21:39:20 +0100

    feat: Update registration screen

--- app/src/main/java/com/example/evenmate/fragments/RegisterFragment.java
+++ app/src/main/java/com/example/evenmate/fragments/RegisterFragment.java
@@ -4,24 +4,38 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.evenmate.R;
-import com.example.evenmate.databinding.FragmentLoginBinding;
 import com.example.evenmate.databinding.FragmentRegisterBinding;
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 public class RegisterFragment extends Fragment {
 
+    private SwitchMaterial switchProvider;
+    private ConstraintLayout companyInfoLayout;
     private FragmentRegisterBinding binding;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         binding = FragmentRegisterBinding.inflate(inflater, container, false);
+
+        switchProvider = binding.switchProvider;
+        companyInfoLayout = binding.companyInfoLayout;
+
+        switchProvider.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                companyInfoLayout.setVisibility(View.VISIBLE);
+            } else {
+                companyInfoLayout.setVisibility(View.GONE);
+            }
+        });
+
         return binding.getRoot();
     }
 
