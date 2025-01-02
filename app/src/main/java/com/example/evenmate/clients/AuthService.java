90110ec08d5255d47554e246882f57af3d6e746b
zoricnadja <zoric.n2003@gmail.com>
2025-01-02 01:34:23 +0100

    feat: Connect to backend login endpoint

--- app/src/main/java/com/example/evenmate/clients/AuthService.java
+++ app/src/main/java/com/example/evenmate/clients/AuthService.java
@@ -1,17 +1,30 @@
 package com.example.evenmate.clients;
 
+import static com.example.evenmate.clients.ClientUtils.BASE_URL;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
-import com.example.evenmate.models.User;
+
+import com.example.evenmate.models.user.LoginRequest;
+import com.example.evenmate.models.user.TokenResponse;
+import com.example.evenmate.models.user.User;
 
 public interface AuthService {
 
     @Headers({
             "User-Agent: Mobile-Android",
-            "Content-Type:application/json"
+            "Content-Type:application/json",
+            "X-Base-URL: " + BASE_URL
     })
     @POST("auth/register")
-    Call<User> registerUser(@Body User user);
+    Call<User> register(@Body User request);
+
+    @Headers({
+            "User-Agent: Mobile-Android",
+            "Content-Type:application/json",
+    })
+    @POST("auth/login")
+    Call<TokenResponse> login(@Body LoginRequest request);
 }
