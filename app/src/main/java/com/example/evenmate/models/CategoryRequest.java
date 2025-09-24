83e853993927a0fdc49ff893733f902a979759ca
DamjanVincic <damjanvincic2003@gmail.com>
2025-09-24 21:51:19 +0200

    feat: add category dtos

---
+++ app/src/main/java/com/example/evenmate/models/CategoryRequest.java
@@ -0,0 +1,16 @@
+package com.example.evenmate.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryRequest {
+    private String name;
+    private String description;
+}
+
