d310457b461d466fac498d0587c9c50dd2e8e3ef
DamjanVincic <damjanvincic2003@gmail.com>
2025-09-24 21:45:24 +0200

    feat: update category service

--- app/src/main/java/com/example/evenmate/clients/CategoryService.java
+++ app/src/main/java/com/example/evenmate/clients/CategoryService.java
@@ -1,12 +1,21 @@
 package com.example.evenmate.clients;
 
 import com.example.evenmate.models.Category;
+import com.example.evenmate.models.CategoryRequest;
+import com.example.evenmate.models.CategorySuggestion;
+import com.example.evenmate.models.PaginatedResponse;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface CategoryService {
     @Headers({
@@ -14,5 +23,17 @@ public interface CategoryService {
             "Content-Type:application/json"
     })
     @GET("categories")
-    Call<List<Category>> getCategories();
+    Call<PaginatedResponse<Category>> getCategories(@Query("page") Integer page, @Query("size") Integer size);
+
+    @POST("categories")
+    Call<Category> createCategory(@Body CategoryRequest request);
+
+    @PUT("categories/{id}")
+    Call<Category> updateCategory(@Path("id") Long id, @Body CategoryRequest request);
+
+    @DELETE("categories/{id}")
+    Call<Void> deleteCategory(@Path("id") Long id);
+
+    @GET("categories/suggestions")
+    Call<PaginatedResponse<CategorySuggestion>> getCategorySuggestions(@Query("page") Integer page, @Query("size") Integer size);
 }
