9d7509c9470814654b9eb6f03c84f665f1308490
zoricnadja <zoric.n2003@gmail.com>
2025-09-24 11:52:23 +0200

    feat: Update menus

---
+++ app/src/main/java/com/example/evenmate/fragments/asset/ProductsFragment.java
@@ -0,0 +1,30 @@
+package com.example.evenmate.fragments.asset;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.evenmate.R;
+
+public class ProductsFragment extends Fragment {
+
+    public ProductsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_products, container, false);
+    }
+}
