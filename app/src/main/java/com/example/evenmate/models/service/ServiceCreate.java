f732195d9ed148fea2a0bba1db06aa0194d0fab0
DamjanVincic <damjanvincic2003@gmail.com>
2025-09-27 22:57:16 +0200

    feat: update service create model

--- app/src/main/java/com/example/evenmate/models/service/ServiceCreate.java
+++ app/src/main/java/com/example/evenmate/models/service/ServiceCreate.java
@@ -5,17 +5,24 @@ import androidx.annotation.Nullable;
 import java.io.Serializable;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ServiceCreate implements Serializable {
     private String name;
     private String description;
     private Double price;
     private Integer discount;
+
+    @Nullable
     private List<String> images;
+
     private Boolean isVisible;
     private Boolean isAvailable;
 
