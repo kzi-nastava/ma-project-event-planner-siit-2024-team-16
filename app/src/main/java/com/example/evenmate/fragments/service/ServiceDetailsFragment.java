1637a71e645a171e31631ac6f7542d5714e37751
RisticAndjela <andjelaristic371@gmail.com>
2025-09-28 04:00:01 +0200

    feat: reservation implementation

--- app/src/main/java/com/example/evenmate/fragments/service/ServiceDetailsFragment.java
+++ app/src/main/java/com/example/evenmate/fragments/service/ServiceDetailsFragment.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.evenmate.R;
+import com.example.evenmate.fragments.reservations.ReservationFragment;
+import com.example.evenmate.models.asset.Service;
 
 public class ServiceDetailsFragment extends Fragment {
 
@@ -31,6 +34,27 @@ public class ServiceDetailsFragment extends Fragment {
             @Nullable ViewGroup container,
             @Nullable Bundle savedInstanceState
     ) {
-        return inflater.inflate(R.layout.fragment_service_details, container, false);
+        View view = inflater.inflate(R.layout.fragment_service_details, container, false);
+
+        Button reserveButton = view.findViewById(R.id.reserve_button);
+        reserveButton.setOnClickListener(v -> openReservationFragment());
+
+        return view;
+    }
+
+    private void openReservationFragment() {
+        Bundle bundle = new Bundle();
+        Service service = new Service();
+        service.setId(serviceId);
+        bundle.putLong("service_id", service.getId());
+
+        ReservationFragment fragment = new ReservationFragment();
+        fragment.setArguments(bundle);
+
+        requireActivity().getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
